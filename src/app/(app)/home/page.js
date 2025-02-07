e26e6a3808b848f60df7043f3d00556232fdf8f8
oscarzhpersson <oscar.zhpersson@gmail.com>
2025-02-07 21:51:14 +0100

    Update home page layout and styling with table view and scrollbar improvements

--- src/app/(app)/home/page.js
+++ src/app/(app)/home/page.js
@@ -1,16 +1,48 @@
+import React from 'react';
+
 import Navbar from '@/components/navbar';
+import Dropdown from '@/components/dropdown';
+import TableCard from '@/components/defined/table-card';
+import GuestCard from '@/components/defined/guest-card';
 
 export default async function HomePage() {
-  const options = [
+  const servers = [
     'Oscar Persson',
     'Anthony Bassey',
     'E Joon Ko',
     'Robin Ellingsen',
   ];
 
+  const options = [
+    'filter1',
+    'filter2',
+    'filter3',
+    'filter4',
+    'filter5',
+    'filter6',
+  ];
+
+  const tables = Array.from({ length: 12 }, (_, index) => ({
+    tableNumber: 500 + index,
+    time: Math.floor(Math.random() * 23) + 1 + ':00',
+    size: Math.floor(Math.random() * 10) + 1,
+  }));
+
   return (
-    <div>
-      <Navbar servers={options} />
+    <div className="flex flex-col">
+      <Navbar servers={servers} />
+      <div className="flex w-1/4 h-screen overflow-y-scroll p-6 pt-24 flex-col space-y-6 border-r border-gray-200 hide-scrollbar">
+        <h2 className="text-lg font-semibold text-black">Tables</h2>
+        <Dropdown options={options} />
+        {tables.map((table) => (
+          <TableCard
+            key={table.tableNumber}
+            tableNumber={table.tableNumber}
+            time={table.time}
+            size={table.size}
+          />
+        ))}
+      </div>
     </div>
   );
 }
