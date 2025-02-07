955463fdd04d919baf1087208aaf6358bf971fb6
E-Joon Ko <e.joon.ko@umain.com>
2025-02-07 21:27:55 +0100

    integrated firebase to project

--- src/app/(app)/home/page.js
+++ src/app/(app)/home/page.js
@@ -1,4 +1,6 @@
 import Navbar from '@/components/navbar';
+import { getFirestore, collection, getDocs } from 'firebase/firestore';
+import firebaseApp from '@/lib/firebase';
 
 export default async function HomePage() {
   const options = [
@@ -7,6 +9,12 @@ export default async function HomePage() {
     'E Joon Ko',
     'Robin Ellingsen',
   ];
+
+  const db = getFirestore(firebaseApp);
+
+  const querySnapshot = await getDocs(collection(db, 'sessions'));
+  const dataList = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+  console.log(dataList)
 
   return (
     <div>
