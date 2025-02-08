ca58c3af64c4a40c438f87ad931d12ee56d5846c
E-Joon Ko <e.joon.ko@umain.com>
2025-02-08 14:25:03 +0100

    expanded addOrder

--- src/api/add-order.js
+++ src/api/add-order.js
@@ -1,6 +1,6 @@
 "use client"
 
-import { getFirestore, doc, collection, addDoc } from 'firebase/firestore';
+import { getFirestore, doc, collection, addDoc, getDocs, query, where, updateDoc } from 'firebase/firestore';
 import firebaseApp from '@/lib/firebase';
 
 const db = getFirestore(firebaseApp);
@@ -16,8 +16,25 @@ export async function addOrder(sessionId, guestId, item, quantity) {
     }
 
     const parentDocRef = doc(db, "sessions", sessionId);
-
     const subcollectionRef = collection(parentDocRef, "orders");
 
-    await addDoc(subcollectionRef, orderPayload);
+    const q = query(
+        subcollectionRef,
+        where("user_id", "==", guestId),
+        where("item_name", "==", item.name),
+        where("status", "==", "pending")
+    );
+
+    const querySnapshot = await getDocs(q);
+
+    if (!querySnapshot.empty) {
+        querySnapshot.forEach(async (docSnapshot) => {
+            const existingOrder = docSnapshot.data();
+            const newQuantity = existingOrder.quantity + quantity;
+
+            await updateDoc(docSnapshot.ref, { quantity: newQuantity });
+        });
+    } else {
+        await addDoc(subcollectionRef, orderPayload);
+    }
 }
